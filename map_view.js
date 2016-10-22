7392a69f199669c08e7411eabfe50589da77e442
WenbinZhu <zwbhsefz@gmail.com>
2016-10-22 12:15:38 -0700

    Create map_view.js

---
+++ map_view.js
@@ -0,0 +1,6 @@
+var map;
+function loadMapScenario() {
+    map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
+        credentials: 'YAtzePqCL6lUAr5H98sJ6JHEYBRy5w2ryt_nIsLv3DrpQglPNZoUSEbyB_dWLFHpd'
+    });
+}
