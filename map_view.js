8e6082b0905d965d177ed6b992fda249a42236ea
WenbinZhu <zwbhsefz@gmail.com>
2016-10-24 00:44:38 -0700

    Update map_view.js

--- map_view.js
+++ map_view.js
@@ -1,15 +1,10 @@
-var map;
-function loadMapScenario() {
-    map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
-        credentials: 'AtzePqCL6lUAr5H98sJ6JHEYBRy5w2ryt_nIsLv3DrpQglPNZoUSEbyB_dWLFHpd'
-    });
-    
-    add_pokemon_layer();
+var map_manager = {
+    map : null,
+    map_items : []
 }
 
-
 //define pokemon data format
-map_items = [
+map_manager.map_items = [
     {
         "pokemon_id" : 4,
         "expire" : 1477296019000,
@@ -17,6 +12,14 @@ map_items = [
         "longitude" : -117.210426
     }
 ]
+
+function loadMapScenario() {
+    map_manager.map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
+        credentials: 'AtzePqCL6lUAr5H98sJ6JHEYBRy5w2ryt_nIsLv3DrpQglPNZoUSEbyB_dWLFHpd'
+    });
+    
+    add_pokemon_layer();
+}
 
 function get_count_down_time_from_expire_epoch(epoch) {
     var now_time = new Date().getTime() / 1000;
@@ -33,8 +36,8 @@ function get_count_down_time_from_expire_epoch(epoch) {
 //put pokemon image on map
 function get_pokemon_layer_from_map_items(map_items) {
     var pushpins = [];
-    for (var i in map_items) {
-        map_item = map_items[i];
+    for (var i in map_manager.map_items) {
+        map_item = map_manager.map_items[i];
         var pushpin = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(map_item["latitude"], map_item["longitude"]), 
                                                 { icon: "images/pushpin_images/pokemon/" + map_item["pokemon_id"] + ".png",
                                                   title: get_count_down_time_from_expire_epoch(map_item["expire"]) });
@@ -48,16 +51,16 @@ function get_pokemon_layer_from_map_items(map_items) {
 }
 
 function add_pokemon_layer() {
-    var pokemon_layer = get_pokemon_layer_from_map_items(map_items);
-    map.layers.insert(pokemon_layer);
+    var pokemon_layer = get_pokemon_layer_from_map_items(map_manager.map_items);
+    map_manager.map.layers.insert(pokemon_layer);
 }
 
 
 //add pokemon count down refresh
 function refresh_pokemon_layer() {
-    var pokemon_layer = get_pokemon_layer_from_map_items(map_items);
-    map.layers.clear();
-    map.layers.insert(pokemon_layer);
+    var pokemon_layer = get_pokemon_layer_from_map_items(map_manager.map_items);
+    map_manager.map.layers.clear();
+    map_manager.map.layers.insert(pokemon_layer);
 }
 
 
