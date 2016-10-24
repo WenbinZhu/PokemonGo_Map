f9fe957edc04e291985f6a972692398924d53158
WenbinZhu <zwbhsefz@gmail.com>
2016-10-24 00:24:45 -0700

    Update map_view.js

--- map_view.js
+++ map_view.js
@@ -18,6 +18,17 @@ map_items = [
     }
 ]
 
+function get_count_down_time_from_expire_epoch(epoch) {
+    var now_time = new Date().getTime() / 1000;
+    var time_left = epoch / 1000 - now_time;
+    var minute = Math.floor(time_left / 60);
+    var second = Math.floor(time_left % 60);
+    if (second < 10) {
+        second = "0" + second;
+    }
+    
+    return minute + ":" + second;
+}
 
 //put pokemon image on map
 function get_pokemon_layer_from_map_items(map_items) {
@@ -25,7 +36,8 @@ function get_pokemon_layer_from_map_items(map_items) {
     for (var i in map_items) {
         map_item = map_items[i];
         var pushpin = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(map_item["latitude"], map_item["longitude"]), 
-                                                { icon: "images/pushpin_images/pokemon/" + map_item["pokemon_id"] + ".png" });
+                                                { icon: "images/pushpin_images/pokemon/" + map_item["pokemon_id"] + ".png",
+                                                  title: get_count_down_time_from_expire_epoch(map_item["expire"]) });
         pushpins.push(pushpin);
     }
     
