dd6b78c50a4f86f0a150a4727c5682b84caae6d2
WenbinZhu <zwbhsefz@gmail.com>
2016-10-23 17:24:27 -0700

    Update map_view.js

--- map_view.js
+++ map_view.js
@@ -11,7 +11,7 @@ function loadMapScenario() {
 //define pokemon data format
 map_items = [
     {
-        "pokemon_id" : 12,
+        "pokemon_id" : 5,
         "expire" : 1476338470000,
         "latitude" : 32.871181,
         "longitude" : -117.210763
@@ -21,7 +21,10 @@ map_items = [
 
 //put pokemon image on map
 function get_pokemon_layer_from_map_items(map_items) {
-    var pushpins = Microsoft.Maps.TestDataGenerator.getPushpins(10, map.getBounds());
+    var pushpins = [];
+    var pushpin = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(map_items["latitude"], map_items["longitude"]), 
+                                             { icon: "images/pushpin_images/pokemon/" + map_items["pokemon_id"] + ".png" });
+    pushpins.push(pushpin);
     var layer = new Microsoft.Maps.Layer();
     layer.add(pushpins);
     
